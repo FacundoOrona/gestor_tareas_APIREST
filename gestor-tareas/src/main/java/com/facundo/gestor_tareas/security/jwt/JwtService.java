c38e072819d5aaa2a2a85da6a4dedeee100a899f
FacundoOrona <fcndorona@gmail.com>
2025-06-05 18:08:53 -0300

    isTokenValid y sus funciones complementarias

--- gestor-tareas/src/main/java/com/facundo/gestor_tareas/security/jwt/JwtService.java
+++ gestor-tareas/src/main/java/com/facundo/gestor_tareas/security/jwt/JwtService.java
@@ -30,6 +30,24 @@ public class JwtService {
                 .getSubject();
     }
 
+    private boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return  username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration();
+    }
+
     private Key getSignInKey() {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
     }
