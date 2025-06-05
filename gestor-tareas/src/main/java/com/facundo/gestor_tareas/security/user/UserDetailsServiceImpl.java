408a07c7819681e2e471310b961252af011bcd49
FacundoOrona <fcndorona@gmail.com>
2025-06-05 17:38:50 -0300

    Update UserDetailsServiceImpl.java

--- gestor-tareas/src/main/java/com/facundo/gestor_tareas/security/user/UserDetailsServiceImpl.java
+++ gestor-tareas/src/main/java/com/facundo/gestor_tareas/security/user/UserDetailsServiceImpl.java
@@ -1,5 +1,25 @@
 package com.facundo.gestor_tareas.security.user;
+import com.facundo.gestor_tareas.entities.Usuario;
+import com.facundo.gestor_tareas.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
 
-public class UserDetailsServiceImpl {
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername (String email) throws UsernameNotFoundException {
+        Usuario user = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado con el email: " + email));
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getContraseña())
+                .roles(user.getRol().name())
+                .build();
+    }
     
 }
