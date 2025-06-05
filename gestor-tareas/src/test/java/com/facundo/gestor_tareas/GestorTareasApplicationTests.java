bcd12962c87b0528409925c83caa4b92a4912da0
FacundoOrona <fcndorona@gmail.com>
2025-06-05 16:04:51 -0300

    config application.properties

--- gestor-tareas/src/test/java/com/facundo/gestor_tareas/GestorTareasApplicationTests.java
+++ gestor-tareas/src/test/java/com/facundo/gestor_tareas/GestorTareasApplicationTests.java
@@ -1,13 +1,13 @@
 package com.facundo.gestor_tareas;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class GestorTareasApplicationTests {
+//@SpringBootTest
+//class GestorTareasApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
-}
+//}
