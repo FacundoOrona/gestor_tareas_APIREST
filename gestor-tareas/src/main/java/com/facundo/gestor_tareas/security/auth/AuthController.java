61e99135bbae89790f4e49e48599658a9b7d0552
FacundoOrona <fcndorona@gmail.com>
2025-06-05 18:18:46 -0300

    endpoint "/register"

--- gestor-tareas/src/main/java/com/facundo/gestor_tareas/security/auth/AuthController.java
+++ gestor-tareas/src/main/java/com/facundo/gestor_tareas/security/auth/AuthController.java
@@ -1,5 +1,39 @@
 package com.facundo.gestor_tareas.security.auth;
 
+import com.facundo.gestor_tareas.entities.Usuario;
+import com.facundo.gestor_tareas.repository.UsuarioRepository;
+import com.facundo.gestor_tareas.security.jwt.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UsuarioRepository usuarioRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final com.facundo.gestor_tareas.security.user.UserDetailsServiceImpl userDetailsService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        Usuario user = Usuario.builder()
+                .nombre(request.getNombre())
+                .email(request.getEmail())
+                .contraseña(passwordEncoder.encode(request.getContraseña()))
+                .rol(Usuario.Rol.MIEMBRO)
+                .build();
+        usuarioRepository.save(user);
+        
+        UserDetails userDetails = userDetailsService.loadUserByUsername(request.getEmail());
+        String jwt = jwtService.generateToken(userDetails);
+        return ResponseEntity.ok(new AuthResponse(jwt));
+    }
     
 }
