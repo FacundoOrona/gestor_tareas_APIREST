78438078f90bbe37f213019930f1e801c3093a98
FacundoOrona <fcndorona@gmail.com>
2025-06-05 16:32:36 -0300

    Create Tarea.java

---
+++ gestor-tareas/src/main/java/com/facundo/gestor_tareas/entities/Tarea.java
@@ -0,0 +1,56 @@
+package com.facundo.gestor_tareas.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "tareas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Tarea {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+
+    @Column(columnDefinition = "TEXT")
+    private String descripcion;
+
+    @Enumerated(EnumType.STRING)
+    private Estado estado;
+
+    @Enumerated(EnumType.STRING)
+    private Prioridad prioridad;
+
+    private LocalDate fechaLimite;
+
+    private String etiquetas;
+
+    @ManyToOne
+    @JoinColumn(name = "responsable_id")
+    private Usuario responsable;
+
+    @ManyToOne
+    @JoinColumn(name = "proyecto_id")
+    private Proyecto proyecto;
+
+    public enum Estado {
+        PENDIENTE,
+        EN_PROGRESO,
+        FINALIZADA,
+    }
+
+    public enum Prioridad {
+        BAJA,
+        MEDIA,
+        ALTA,
+    }
+
+}
