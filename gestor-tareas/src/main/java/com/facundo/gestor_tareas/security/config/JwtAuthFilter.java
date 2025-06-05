fc5d207940189af0ab55c24ed5d7024b6d6088ae
FacundoOrona <fcndorona@gmail.com>
2025-06-05 18:45:39 -0300

    Create JwtAuthFilter.java

---
+++ gestor-tareas/src/main/java/com/facundo/gestor_tareas/security/config/JwtAuthFilter.java
@@ -0,0 +1,19 @@
+package com.facundo.gestor_tareas.security.config;
+
+import com.facundo.gestor_tareas.security.user.UserDetailsServiceImpl;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.*;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter{
+    
+}
