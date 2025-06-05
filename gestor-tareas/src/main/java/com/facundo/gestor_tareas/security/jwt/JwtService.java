2cea5004e7377c980c9fdb6a9195a4318c100704
FacundoOrona <fcndorona@gmail.com>
2025-06-05 17:58:42 -0300

    generateToken impl

--- gestor-tareas/src/main/java/com/facundo/gestor_tareas/security/jwt/JwtService.java
+++ gestor-tareas/src/main/java/com/facundo/gestor_tareas/security/jwt/JwtService.java
@@ -1,5 +1,27 @@
 package com.facundo.gestor_tareas.security.jwt;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import java.security.Key;
+import java.util.Date;
+
+@Service
 public class JwtService {
     
+    private static final String SECRET_KEY = " ";
+
+    public String generateToken(UserDetails userDetails) {
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 24 horas
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    private Key getSignInKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
 }
