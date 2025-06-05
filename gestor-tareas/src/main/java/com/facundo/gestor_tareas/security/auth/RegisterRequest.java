b5b00588b261e13e3e45f582f086ca704b6624e8
FacundoOrona <fcndorona@gmail.com>
2025-06-05 17:13:07 -0300

    RegisterRequest

--- gestor-tareas/src/main/java/com/facundo/gestor_tareas/security/auth/RegisterRequest.java
+++ gestor-tareas/src/main/java/com/facundo/gestor_tareas/security/auth/RegisterRequest.java
@@ -1,5 +1,10 @@
 package com.facundo.gestor_tareas.security.auth;
 
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
-    
+    private String nombre;
+    private String email;
+    private String contraseña;    
 }
