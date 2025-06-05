a58183775d3a26920dcca590d3bc04e6b23ab5a4
FacundoOrona <fcndorona@gmail.com>
2025-06-05 19:06:05 -0300

    securityFilterChain

--- gestor-tareas/src/main/java/com/facundo/gestor_tareas/security/config/SecurityConfig.java
+++ gestor-tareas/src/main/java/com/facundo/gestor_tareas/security/config/SecurityConfig.java
@@ -1,5 +1,36 @@
 package com.facundo.gestor_tareas.security.config;
 
+import com.facundo.gestor_tareas.security.config.JwtAuthFilter;
+import com.facundo.gestor_tareas.security.user.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.*;
+import org.springframework.security.authentication.*;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
     
+    private final JwtAuthFilter jwtAuthFilter;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .userDetailsService(userDetailsService)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 }
