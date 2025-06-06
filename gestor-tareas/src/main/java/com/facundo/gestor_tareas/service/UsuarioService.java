b82640a998a63f201c2fce4aa8f06f6251bf0c24
FacundoOrona <fcndorona@gmail.com>
2025-06-06 16:25:18 -0300

    usuarioService interface findall / findbyid

---
+++ gestor-tareas/src/main/java/com/facundo/gestor_tareas/service/UsuarioService.java
@@ -0,0 +1,10 @@
+package com.facundo.gestor_tareas.service;
+
+import com.facundo.gestor_tareas.entities.Usuario;
+import java.util.List;
+import java.util.Optional;
+
+public interface UsuarioService {
+    List<Usuario> obtenerTodos();
+    Optional<Usuario> obtenerPorId(Long id);
+}
