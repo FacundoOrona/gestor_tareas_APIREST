c842799e1709e2e0729e6210d9c32b96b623ee76
FacundoOrona <fcndorona@gmail.com>
2025-06-06 16:26:39 -0300

    controller endpoints obtenertodos y por id

---
+++ gestor-tareas/src/main/java/com/facundo/gestor_tareas/controller/UsuarioController.java
@@ -0,0 +1,29 @@
+package com.facundo.gestor_tareas.controller;
+
+import com.facundo.gestor_tareas.entities.Usuario;
+import com.facundo.gestor_tareas.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+    
+    private final UsuarioService usuarioService;
+
+    @GetMapping
+    public List<Usuario> obtenerTodos() {
+        return usuarioService.obtenerTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+        return usuarioService.obtenerPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+}
