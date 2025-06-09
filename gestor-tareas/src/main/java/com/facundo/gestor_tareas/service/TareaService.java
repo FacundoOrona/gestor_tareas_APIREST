103aa50b29c95c76606466147ea7a31caacf7bdd
FacundoOrona <fcndorona@gmail.com>
2025-06-09 15:35:09 -0300

    Create TareaService.java

---
+++ gestor-tareas/src/main/java/com/facundo/gestor_tareas/service/TareaService.java
@@ -0,0 +1,11 @@
+package com.facundo.gestor_tareas.service;
+
+import com.facundo.gestor_tareas.entities.Tarea;
+import java.util.List;
+import java.util.Optional;
+
+public interface TareaService {
+    List<Tarea> obtenerTodas();
+    Optional<Tarea> obtenerPorId(Long id);
+    Tarea crearTarea(Tarea tarea);
+}
