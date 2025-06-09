51b236cbd6450403268a70dc1dbe1621f214118f
FacundoOrona <fcndorona@gmail.com>
2025-06-09 15:13:08 -0300

    create interfaceProyectoService

---
+++ gestor-tareas/src/main/java/com/facundo/gestor_tareas/service/ProyectoService.java
@@ -0,0 +1,12 @@
+package com.facundo.gestor_tareas.service;
+
+import com.facundo.gestor_tareas.entities.Proyecto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProyectoService {
+    List<Proyecto> obtenerTodos();
+    Optional<Proyecto> obtenerPorId(Long id);
+    Proyecto crearProyecto(Proyecto proyecto);
+}
