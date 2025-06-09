ff66a73cc73f359d744944bbb8b861c6eff48020
FacundoOrona <fcndorona@gmail.com>
2025-06-09 15:38:12 -0300

    Create TareaServiceImpl.java

---
+++ gestor-tareas/src/main/java/com/facundo/gestor_tareas/service/impl/TareaServiceImpl.java
@@ -0,0 +1,15 @@
+package com.facundo.gestor_tareas.service.impl;
+
+import com.facundo.gestor_tareas.entities.Tarea;
+import com.facundo.gestor_tareas.repository.TareaRepository;
+import com.facundo.gestor_tareas.service.TareaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TareaServiceImpl implements TareaService {
+    
+}
