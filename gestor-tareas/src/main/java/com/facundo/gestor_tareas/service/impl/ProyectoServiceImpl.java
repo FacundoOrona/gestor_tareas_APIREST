027cbc5952867b97e9fd1fc213ccda60d45db5f0
FacundoOrona <fcndorona@gmail.com>
2025-06-09 15:19:18 -0300

    impl obtenerPorId

--- gestor-tareas/src/main/java/com/facundo/gestor_tareas/service/impl/ProyectoServiceImpl.java
+++ gestor-tareas/src/main/java/com/facundo/gestor_tareas/service/impl/ProyectoServiceImpl.java
@@ -1,10 +1,11 @@
 package com.facundo.gestor_tareas.service.impl;
 
 import org.springframework.stereotype.Service;
-
 import com.facundo.gestor_tareas.service.ProyectoService;
-
 import lombok.RequiredArgsConstructor;
+import com.facundo.gestor_tareas.repository.ProyectoRepository;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor;
@@ -16,4 +17,7 @@ public class ProyectoServiceImpl implements ProyectoService {
     public List<Proyecto> obtenerTodos() {
         return proyectoRepository.findAll();
     }
+
+    @Override
+
 }
