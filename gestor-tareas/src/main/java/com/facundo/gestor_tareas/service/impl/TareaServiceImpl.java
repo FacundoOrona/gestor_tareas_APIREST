f008a2e093624ac1e39c1de7c553a9b0fa05bf2e
FacundoOrona <fcndorona@gmail.com>
2025-06-18 15:29:25 -0300

    actualizarTarea serviceImpl

--- gestor-tareas/src/main/java/com/facundo/gestor_tareas/service/impl/TareaServiceImpl.java
+++ gestor-tareas/src/main/java/com/facundo/gestor_tareas/service/impl/TareaServiceImpl.java
@@ -28,4 +28,34 @@ public class TareaServiceImpl implements TareaService {
     public Tarea crearTarea(Tarea tarea) {
         return tareaRepository.save(tarea);
     }
+
+    @Override
+    public Tarea actualizarTarea(Long id, Tarea tareaActualizada) {
+        return tareaRepository.findById(id)
+                .map(tareaExistente -> {
+                if(tareaActualizada.getTitulo() != null && !tareaActualizada.getTitulo().isBlank()){
+                    tareaExistente.setTitulo(tareaActualizada.getTitulo());
+                }
+                if (tareaActualizada.getDescripcion() != null && !tareaActualizada.getDescripcion().isBlank()) {
+                    tareaExistente.setDescripcion(tareaActualizada.getDescripcion());
+                }
+                if (tareaActualizada.getEstado() != null) {
+                    tareaExistente.setEstado(tareaActualizada.getEstado());
+                }
+                if (tareaActualizada.getPrioridad() != null) {
+                    tareaExistente.setPrioridad(tareaActualizada.getPrioridad());
+                }
+                if (tareaActualizada.getFechaLimite() != null) {
+                    tareaExistente.setFechaLimite(tareaActualizada.getFechaLimite());
+                }
+                if (tareaActualizada.getResponsable() != null) {
+                    tareaExistente.setResponsable(tareaActualizada.getResponsable());
+                }
+                if (tareaActualizada.getProyecto() != null) {
+                    tareaExistente.setProyecto(tareaActualizada.getProyecto());
+                }
+                return tareaRepository.save(tareaExistente);
+                })
+                .orElseThrow(() -> new RuntimeException("Tarea no encontrada"));
+    }
 }
