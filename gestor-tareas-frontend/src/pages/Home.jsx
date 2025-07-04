dbf57001e792e77dbe1503be1997633a60c4f77b
FacundoOrona <fcndorona@gmail.com>
2025-07-04 16:46:51 -0300

    create home, login & register pages

---
+++ gestor-tareas-frontend/src/pages/Home.jsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>Bienvenido al Gestor de Tareas</h1>;
+}
---
+++ gestor-tareas-frontend/src/pages/Register.jsx
@@ -0,0 +1,8 @@
+export default function Register() {
+  return (
+    <div>
+      <h1>Registro</h1>
+      {/* Formulario de registro aquí */}
+    </div>
+  );
+}
