6fa9daf73463f995d59e757b0c42b4cb335a7a0a
FacundoOrona <fcndorona@gmail.com>
2025-07-07 17:38:38 -0300

    findByCreadorEmail

--- gestor-tareas/src/main/java/com/facundo/gestor_tareas/repository/ProyectoRepository.java
+++ gestor-tareas/src/main/java/com/facundo/gestor_tareas/repository/ProyectoRepository.java
@@ -1,9 +1,12 @@
 package com.facundo.gestor_tareas.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.facundo.gestor_tareas.entities.Proyecto;
 
 
 public interface ProyectoRepository extends JpaRepository<Proyecto, Long> {
+
+    List<Proyecto> findByCreadorEmail(String email);
 
 }
