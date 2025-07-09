26b31bb783b6a0278c0c4049700562547d048e73
FacundoOrona <fcndorona@gmail.com>
2025-07-09 15:23:42 -0300

    Update Home.jsx

--- gestor-tareas-frontend/src/pages/Home/Home.jsx
+++ gestor-tareas-frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,16 @@
 import styles from './Home.module.scss';
 import { motion } from 'framer-motion';
 import tareasImg from '../../assets/tareas.svg';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
+  const usuario = localStorage.getItem('usuario') || 'Invitado';
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/misproyectos');
+  };
+
   return (
     <div className={styles.home}>
       <motion.div
@@ -11,10 +19,20 @@ export default function Home() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
       >
+        <h2 className={styles.bienvenida}>Bienvenido {usuario}</h2>
+
         <img src={tareasImg} alt="Bienvenida" className={styles.illustration} />
         <h1>Bienvenido a <span className={styles.brand}>Gestor de Tareas</span></h1>
         <p className="text-muted">Organizá tu día. Prioridades. Tiempo. Todo desde un solo lugar.</p>
+
+        <button
+          className={styles.botonProyectos}
+          onClick={handleClick}
+        >
+          Gestionar mis proyectos
+        </button>
       </motion.div>
     </div>
   );
 }
+
