0b86ae5a547c392f651063def5a583d4bb82cc3e
FacundoOrona <fcndorona@gmail.com>
2025-07-17 17:13:35 -0300

    Update App.jsx

--- gestor-tareas-frontend/src/App.jsx
+++ gestor-tareas-frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import AppRoutes from "./routes";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <div style={{ padding: "2rem" }}>
-        <AppRoutes />
-      </div>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <div style={{ padding: "2rem" }}>
+          <AppRoutes />
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
